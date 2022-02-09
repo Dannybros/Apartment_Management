22c7dcffab1de24bf0d71c83f3b5af7a624b1515
Danny Bros <akbaromega89@gmail.com>
2022-02-09 18:46:03 +0700

    start of staff page

--- dashboard/staff.php
+++ dashboard/staff.php
@@ -63,6 +63,49 @@
                     </div>
                 </div>
             </div>
+
+            <!-- Change Shift -->
+            <div class="modal fade" id="ChangeShift" tabindex="-1" role="dialog" aria-labelledby="ChangeShiftLabel" aria-hidden="true">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                            ...
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-primary">Save</button>
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Edit Staff Info -->
+            <div class="modal fade" id="ChangeShift" tabindex="-1" role="dialog" aria-labelledby="ChangeShiftLabel" aria-hidden="true">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                            ...
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-primary">Save</button>
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
        </div>
     </div>
 </div>
